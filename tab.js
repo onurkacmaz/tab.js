cb4578ba3a851c4ef4ffcfd71cf3a827fc2c9022
Onur Kaçmaz <kacmaz.onur@hotmail.com>
2017-02-09 12:58:47 +0200

    Add files via upload

---
+++ tab.js
@@ -0,0 +1,12 @@
+function tab() {
+	$('ul.tabs li').click(function(){
+		var tab_id = $(this).attr('data-tab');
+
+		$('ul.tabs li').removeClass('current');
+		$('.tab-content').removeClass('current');
+
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+	})
+}
+setTimeout("tab()", 100);
